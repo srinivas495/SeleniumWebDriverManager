d3ea3f855a8b21d1a9149714dfa76d72fce27525
Srinivas <srinivaschari.y@gmail.com>
2021-02-08 21:04:58 +0530

    SeleniumWebDriverManager

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		driver.close();
+	}
+
+}
